93313f7919e3e4d84d68d73bfe386570094f3b80
Tymoteusz Makowski <tymek.makowski@gmail.com>
2020-07-03 21:40:51 +0200

    Added ranger's rifle.conf config.

--- ranger/install.sh
+++ ranger/install.sh
@@ -2,3 +2,8 @@
 echo -n "\e[36m[ranger]\e[0m "
 script_dir=$(dirname $0)
 sh $script_dir/../symlink_config.sh $script_dir/rc.conf $HOME/.config/ranger
+
+# Linking rifle.conf file
+echo -n "\e[36m[ranger]\e[0m "
+script_dir=$(dirname $0)
+sh $script_dir/../symlink_config.sh $script_dir/rifle.conf $HOME/.config/ranger
