4b3719285b58846cb6b4754a7fd1f7d549cbf18d
Tymoteusz Makowski <tymek.makowski@gmail.com>
2020-07-03 12:13:33 +0200

    Added theme to kitty.

--- kitty/install.sh
+++ kitty/install.sh
@@ -2,3 +2,8 @@
 echo -n "\e[36m[kitty]\e[0m "
 script_dir=$(dirname $0)
 sh $script_dir/../symlink_config.sh $script_dir/kitty.conf $HOME/.config/kitty
+
+# Linking theme.conf file
+echo -n "\e[36m[kitty]\e[0m "
+script_dir=$(dirname $0)
+sh $script_dir/../symlink_config.sh $script_dir/theme.conf $HOME/.config/kitty
