40149bcefca91702aa936e07eacccb126b1d937b
Tymoteusz Makowski <tymek.makowski@gmail.com>
2021-06-28 10:12:18 +0200

    Remove PowerShell and Windows terminal

--- win-term/install.bat
+++
@@ -1,3 +0,0 @@
-copy /-Y settings.json %LOCALAPPDATA%\packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState
-copy /-Y ubuntu-icon.png %LOCALAPPDATA%\packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState
-copy /-Y aliases.bat %USERPROFILE%
