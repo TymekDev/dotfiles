22c3d8b3cfefad5351baa935ff935b1cd9655d6d
Tymoteusz Makowski <tymek.makowski@gmail.com>
2022-01-07 20:23:56 +0100

    feat: add packages without configs

--- home.nix
+++ home.nix
@@ -6,5 +6,51 @@
     homeDirectory = "/home/tmakowski";
     stateVersion = "22.05";
   };
+
+  home.packages = with pkgs; [
+    asciinema
+    bat
+    betterlockscreen
+    blueman
+    curl
+    delta
+    entr
+    feh
+    # TODO: firefox
+    fish
+    flameshot
+    fzf
+    gimp
+    git
+    go_1_17
+    htop
+    hugo
+    i3
+    i3status
+    jq
+    kazam
+    kitty
+    moreutils
+    mpv
+    ncdu
+    pandoc
+    pavucontrol
+    python3
+    ripgrep
+    # TODO: rofi
+    sqlite
+    tdesktop
+    tree
+    universal-ctags
+    unzip
+    vim
+    wine
+    zip
+    zsh
+    # TODO: install unfree packages
+    # discord
+    # steam
+  ];
+
   programs.home-manager.enable = true;
 }
