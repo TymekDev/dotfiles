e9433feaf7a0da42fce2b1ce3edb75d73d6bf8d4
Tymoteusz Makowski <tymek.makowski@gmail.com>
2022-01-07 20:26:36 +0100

    feat: add kitty configuration

--- home.nix
+++ home.nix
@@ -29,7 +29,6 @@
     i3status
     jq
     kazam
-    kitty
     moreutils
     mpv
     ncdu
@@ -52,5 +51,47 @@
     # steam
   ];
 
-  programs.home-manager.enable = true;
+  programs = {
+    home-manager.enable = true;
+
+    kitty = {
+      enable = true;
+      font = {
+        package = pkgs.jetbrains-mono;
+        name = "JetBrains Mono";
+        size = 12;
+      };
+      extraConfig = ''
+        # https://github.com/rsaihe/gruvbox-material-kitty/blob/main/colors/gruvbox-material-dark-medium.conf
+        background #282828
+        foreground #d4be98
+        selection_background #d4be98
+        selection_foreground #282828
+        cursor #a89984
+        cursor_text_color background
+        active_tab_background #282828
+        active_tab_foreground #d4be98
+        active_tab_font_style bold
+        inactive_tab_background #282828
+        inactive_tab_foreground #a89984
+        inactive_tab_font_style normal
+        color0 #665c54
+        color8 #928374
+        color1 #ea6962
+        color9 #ea6962
+        color2 #a9b665
+        color10 #a9b665
+        color3 #e78a4e
+        color11 #d8a657
+        color4 #7daea3
+        color12 #7daea3
+        color5 #d3869b
+        color13 #d3869b
+        color6 #89b482
+        color14 #89b482
+        color7 #d4be98
+        color15 #d4be98
+      '';
+    };
+  };
 }
