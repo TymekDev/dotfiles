d3ebeaa41b6d62f7a26933a0dacfddfc0be20ac1
Tymoteusz Makowski <tymek.makowski@gmail.com>
2022-01-08 22:41:12 +0100

    feat: add a primitive vim config

--- home.nix
+++ home.nix
@@ -5,6 +5,12 @@
     username = "tmakowski";
     homeDirectory = "/home/tmakowski";
     stateVersion = "22.05";
+    file = {
+      ".vim" = {
+        source = ./.vim;
+        recursive = true;
+      };
+    };
     # List of packages not defined down below
     # NOTE: some packages don't have explicit config definitions (e.g. are used only in i3 keybindings)
     packages = with pkgs; [
