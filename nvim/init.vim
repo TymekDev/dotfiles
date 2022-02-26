c394545459f324aa0cf10254dfa645cefe354bd2
Tymoteusz Makowski <tymek.makowski@gmail.com>
2022-02-26 19:37:40 +0100

    feat: add nvim-cmp

--- nvim/init.vim
+++ nvim/init.vim
@@ -18,6 +18,14 @@ Plug 'unblevable/quick-scope'
 " Neovim exclusive
 Plug 'neovim/nvim-lspconfig'
 
+Plug 'hrsh7th/nvim-cmp'
+Plug 'hrsh7th/cmp-nvim-lsp'
+Plug 'hrsh7th/cmp-buffer'
+Plug 'hrsh7th/cmp-path'
+
+Plug 'L3MON4D3/LuaSnip'
+Plug 'saadparwaiz1/cmp_luasnip'
+
 call plug#end()
 
 
--- nvim/lua/init.lua
+++ nvim/lua/init.lua
@@ -1,4 +1,9 @@
+-- Setup LSP
+
+local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+
 require('lspconfig').gopls.setup{
+  capabilities = capabilities,
   on_attach = function()
     vim.keymap.set('n', 'K', vim.lsp.buf.hover, {buffer=0})
     vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {buffer=0})
@@ -9,3 +14,33 @@ require('lspconfig').gopls.setup{
     vim.keymap.set('n', '<Leader>q', vim.diagnostic.setqflist, {buffer=0})
   end,
 }
+
+
+-- Setup nvim-cmp
+vim.opt.completeopt={"menu", "menuone", "noselect"}
+
+local cmp = require('cmp')
+
+cmp.setup({
+  snippet = {
+    -- REQUIRED - you must specify a snippet engine
+    expand = function(args)
+      require('luasnip').lsp_expand(args.body)
+    end,
+  },
+  mapping = {
+    ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
+    ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
+    ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
+    ['<C-e>'] = cmp.mapping({
+      i = cmp.mapping.abort(),
+      c = cmp.mapping.close(),
+    }),
+    ['<C-j>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+  },
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+  }, {
+    { name = 'buffer' },
+  })
+})
