85c39bdb15f05f7b557713b33d06b70900902d33
Tymoteusz Makowski <tymek.makowski@gmail.com>
2022-03-06 21:20:18 +0100

    style: replace single quotes with double quotes

--- nvim/lua/init.lua
+++ nvim/lua/init.lua
@@ -1,30 +1,29 @@
-require('lsp')
+require("lsp")
 
 -- Setup nvim-cmp
 vim.opt.completeopt={"menu", "menuone", "noselect"}
 
-local cmp = require('cmp')
-
+local cmp = require("cmp")
 cmp.setup({
   snippet = {
     -- REQUIRED - you must specify a snippet engine
     expand = function(args)
-      require('luasnip').lsp_expand(args.body)
+      require("luasnip").lsp_expand(args.body)
     end,
   },
   mapping = {
-    ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
-    ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
-    ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
-    ['<C-e>'] = cmp.mapping({
+    ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
+    ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
+    ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
+    ["<C-e>"] = cmp.mapping({
       i = cmp.mapping.abort(),
       c = cmp.mapping.close(),
     }),
-    ['<C-j>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    ["<C-j>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
   },
   sources = cmp.config.sources({
-    { name = 'nvim_lsp' },
+    { name = "nvim_lsp" },
   }, {
-    { name = 'buffer' },
+    { name = "buffer" },
   })
 })
