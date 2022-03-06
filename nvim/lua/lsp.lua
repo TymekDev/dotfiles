a52d40f6362e07d16d0a11873b674e47240ea32e
Tymoteusz Makowski <t@makowski.sh>
2022-03-06 21:20:18 +0100

    style: replace single quotes with double quotes

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -1,13 +1,13 @@
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 
 local addFormatAutocmd = function()
-  vim.api.nvim_command('autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()')
+  vim.api.nvim_command("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()")
 end
 
 
 -- CSS
-require('lspconfig').stylelint_lsp.setup{
+require("lspconfig").stylelint_lsp.setup{
   capabilities = capabilities,
   on_attach = addFormatAutocmd,
   settings = {
@@ -19,26 +19,26 @@ require('lspconfig').stylelint_lsp.setup{
 
 
 -- Golang
-require('lspconfig').gopls.setup{
+require("lspconfig").gopls.setup{
   capabilities = capabilities,
   on_attach = function()
     addFormatAutocmd()
     -- TODO: explore what other options `vim.lsp.` offers
-    vim.keymap.set('n', 'K', vim.lsp.buf.hover, {buffer=0})
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {buffer=0})
-    vim.keymap.set('n', 'gt', vim.lsp.buf.type_definition, {buffer=0})
-    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, {buffer=0})
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, {buffer=0})
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, {buffer=0})
+    vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, {buffer=0})
+    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {buffer=0})
     -- TODO: explore what other options `vim.diagnostic.` offers
-    vim.keymap.set('n', '<Leader>k', vim.diagnostic.goto_prev, {buffer=0})
-    vim.keymap.set('n', '<Leader>j', vim.diagnostic.goto_next, {buffer=0})
-    vim.keymap.set('n', '<Leader>q', vim.diagnostic.setqflist, {buffer=0})
+    vim.keymap.set("n", "<Leader>k", vim.diagnostic.goto_prev, {buffer=0})
+    vim.keymap.set("n", "<Leader>j", vim.diagnostic.goto_next, {buffer=0})
+    vim.keymap.set("n", "<Leader>q", vim.diagnostic.setqflist, {buffer=0})
     -- TODO: add rename
   end,
 }
 
 
 -- HTML
-require('lspconfig').html.setup{
+require("lspconfig").html.setup{
   capabilities = capabilities,
   on_attach = addFormatAutocmd,
 }
