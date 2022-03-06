10f19f2022e5587ee9ff52f631d9083699cfb968
Tymoteusz Makowski <t@makowski.sh>
2022-03-06 10:35:25 +0100

    refactor: move LSP definitions to lsp.lua

--- nvim/lua/init.lua
+++ nvim/lua/init.lua
@@ -1,31 +1,4 @@
--- Setup LSP
-
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
-
-require('lspconfig').gopls.setup{
-  capabilities = capabilities,
-  on_attach = function()
-    vim.keymap.set('n', 'K', vim.lsp.buf.hover, {buffer=0})
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {buffer=0})
-    vim.keymap.set('n', 'gt', vim.lsp.buf.type_definition, {buffer=0})
-    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, {buffer=0})
-    vim.keymap.set('n', '<Leader>k', vim.diagnostic.goto_prev, {buffer=0})
-    vim.keymap.set('n', '<Leader>j', vim.diagnostic.goto_next, {buffer=0})
-    vim.keymap.set('n', '<Leader>q', vim.diagnostic.setqflist, {buffer=0})
-  end,
-}
-
--- CSS
-require('lspconfig').stylelint_lsp.setup{
-  capabilities = capabilities,
-}
-
--- HTML
-capabilities.textDocument.completion.completionItem.snippetSupport = true
-require('lspconfig').html.setup{
-  capabilities = capabilities,
-}
-
+require('lsp')
 
 -- Setup nvim-cmp
 vim.opt.completeopt={"menu", "menuone", "noselect"}
