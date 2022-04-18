c0984b93405881eeb6685884b708d3c04dc4f103
Tymoteusz Makowski <tymek.makowski@gmail.com>
2022-04-18 20:15:51 +0200

    refactor: create a single on_attach callback function

--- config/nvim/lua/lsp.lua
+++ config/nvim/lua/lsp.lua
@@ -3,15 +3,26 @@ local lspconfig = require("lspconfig")
 local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 
-local addFormatAutocmd = function()
-  vim.api.nvim_command("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()")
+local on_attach = function(client)
+  if client.resolved_capabilities.document_formatting then
+    vim.api.nvim_command("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()")
+  end
+  -- TODO: explore what other options `vim.lsp.` offers
+  vim.keymap.set("n", "K", vim.lsp.buf.hover, {buffer=0})
+  vim.keymap.set("n", "gd", vim.lsp.buf.definition, {buffer=0})
+  vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, {buffer=0})
+  vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {buffer=0})
+  -- TODO: explore what other options `vim.diagnostic.` offers
+  vim.keymap.set("n", "<Leader>k", vim.diagnostic.goto_prev, {buffer=0})
+  vim.keymap.set("n", "<Leader>j", vim.diagnostic.goto_next, {buffer=0})
+  vim.keymap.set("n", "<Leader>q", vim.diagnostic.setqflist, {buffer=0})
+  -- TODO: add rename
 end
 
 
--- CSS
 lspconfig.stylelint_lsp.setup{
   capabilities = capabilities,
-  on_attach = addFormatAutocmd,
+  on_attach = on_attach,
   settings = {
     stylelintplus = {
       autoFixOnFormat = true,
@@ -23,31 +34,19 @@ lspconfig.stylelint_lsp.setup{
 -- Golang
 lspconfig.gopls.setup{
   capabilities = capabilities,
-  on_attach = function()
-    addFormatAutocmd()
-    -- TODO: explore what other options `vim.lsp.` offers
-    vim.keymap.set("n", "K", vim.lsp.buf.hover, {buffer=0})
-    vim.keymap.set("n", "gd", vim.lsp.buf.definition, {buffer=0})
-    vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, {buffer=0})
-    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {buffer=0})
-    -- TODO: explore what other options `vim.diagnostic.` offers
-    vim.keymap.set("n", "<Leader>k", vim.diagnostic.goto_prev, {buffer=0})
-    vim.keymap.set("n", "<Leader>j", vim.diagnostic.goto_next, {buffer=0})
-    vim.keymap.set("n", "<Leader>q", vim.diagnostic.setqflist, {buffer=0})
-    -- TODO: add rename
-  end,
+  on_attach = on_attach,
 }
 
 
 -- HTML
 lspconfig.html.setup{
   capabilities = capabilities,
-  on_attach = addFormatAutocmd,
+  on_attach = on_attach,
 }
 
 
 -- R
 lspconfig.r_language_server.setup{
   capabilities = capabilities,
-  on_attach = addFormatAutocmd,
+  on_attach = on_attach,
 }
