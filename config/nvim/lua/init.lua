2a39a6719997fe7db6fe24c1db689fe5e30663f3
Tymoteusz Makowski <tymek.makowski@gmail.com>
2022-06-07 19:54:01 +0200

    fix: add missing <C-n> and <C-p> mappings

--- config/nvim/lua/init.lua
+++ config/nvim/lua/init.lua
@@ -38,6 +38,8 @@ cmp.setup({
       c = cmp.mapping.close(),
     }),
     ["<C-j>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    ["<C-n>"] = cmp.mapping(cmp.mapping.select_next_item(), {"i", "c"}),
+    ["<C-p>"] = cmp.mapping(cmp.mapping.select_prev_item(), {"i", "c"}),
   },
   sources = cmp.config.sources({
     { name = "nvim_lsp" },
