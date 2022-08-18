c50ef7936b2f4827de8fe42ebbb70eabc2eb3185
Tymoteusz Makowski <tymek.makowski@gmail.com>
2022-08-18 10:38:06 +0200

    refactor(nvim): move options to Lua

---
+++ config/nvim/after/plugin/options.lua
@@ -0,0 +1,25 @@
+local o = vim.opt
+
+o.expandtab = true
+o.history = 1000
+o.hlsearch = false
+o.list = true
+o.listchars:append({ trail = "˽", extends = ">", precedes = "<" })
+o.mouse = nil
+o.number = true
+o.scrolloff = 8
+o.shiftwidth = 2
+o.shortmess:append({ c = true })
+o.sidescrolloff = 5
+o.signcolumn = "yes"
+o.smartindent = true
+o.softtabstop = 2
+o.spelllang = {"en", "pl"}
+o.splitbelow = true
+o.splitright = true
+o.swapfile = false
+o.tabstop = 2
+o.ttimeoutlen = 100
+o.undofile = true
+o.updatetime = 50
+o.wrap = false
