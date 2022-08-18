fff9d7b5390ce03016bc57af1eed3475a57a0445
Tymoteusz Makowski <tymek.makowski@gmail.com>
2022-08-18 23:39:38 +0300

    feat: add cmd option to nnoremap

--- config/nvim/lua/tymek/keymap.lua
+++ config/nvim/lua/tymek/keymap.lua
@@ -1,7 +1,11 @@
 local M = {}
 
 local function nnoremap(lhs, rhs, opts)
-  vim.keymap.set("n", lhs, rhs, opts)
+  if opts ~= nil and opts.cmd then
+    _rhs = function() vim.cmd(rhs) end
+    opts.cmd = nil
+  end
+  vim.keymap.set("n", lhs, _rhs or rhs, opts)
 end
 
 M.nnoremap = nnoremap
