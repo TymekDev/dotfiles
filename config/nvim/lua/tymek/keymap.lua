74324277ed8bf7b2b88602cf43790a2a023d2f08
Tymoteusz Makowski <tymek.makowski@gmail.com>
2022-08-19 17:29:17 +0300

    fix: make nnoremap work

--- config/nvim/lua/tymek/keymap.lua
+++ config/nvim/lua/tymek/keymap.lua
@@ -1,11 +1,12 @@
 local M = {}
 
 local function nnoremap(lhs, rhs, opts)
-  if opts ~= nil and opts.cmd then
+  _rhs = rhs
+  if opts ~= nil and opts.cmd == true then
     _rhs = function() vim.cmd(rhs) end
     opts.cmd = nil
   end
-  vim.keymap.set("n", lhs, _rhs or rhs, opts)
+  vim.keymap.set("n", lhs, _rhs, opts)
 end
 
 M.nnoremap = nnoremap
