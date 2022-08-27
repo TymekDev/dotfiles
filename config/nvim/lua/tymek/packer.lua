569f3aa7415f262b16ea2cc1284b60295c79c99b
Tymoteusz Makowski <tymek.makowski@gmail.com>
2022-08-27 19:34:57 +0200

    fix(nvim): disable signs
    These were messing up lines on my Chromebook.

--- config/nvim/lua/tymek/packer.lua
+++ config/nvim/lua/tymek/packer.lua
@@ -118,7 +118,9 @@ return require("packer").startup(function(use)
     "folke/todo-comments.nvim",
     requires = "nvim-lua/plenary.nvim",
     config = function()
-      require("todo-comments").setup()
+      require("todo-comments").setup({
+        signs = false,
+      })
     end
   }
 
