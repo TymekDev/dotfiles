1fca240d404ab12a614202f47a753b907f3abab4
Tymoteusz Makowski <tymek.makowski@gmail.com>
2022-10-08 21:51:59 +0200

    feat(nvim): add repos configuration

---
+++ config/nvim/after/plugin/repos.lua
@@ -0,0 +1,8 @@
+require("repos").setup({
+  callbacks = {
+    ["sso"] = function(root)
+      vim.opt.makeprg = "make -C " .. root
+      vim.opt.shellpipe = "2>/dev/null | sed 's#^[^[:blank:]]#" .. root .. "/src/&#' | tee"
+    end,
+  },
+})
