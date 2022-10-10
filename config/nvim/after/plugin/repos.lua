351afbfd5ab9401a145fad82bd190db5dbe95b05
Tymoteusz Makowski <tymek.makowski@gmail.com>
2022-10-10 23:29:30 +0200

    feat(nvim): update repos.nvim config

--- config/nvim/after/plugin/repos.lua
+++ config/nvim/after/plugin/repos.lua
@@ -3,6 +3,7 @@ require("repos").setup({
     ["sso"] = function(root)
       vim.opt.makeprg = "make -C " .. root
       vim.opt.shellpipe = "2>/dev/null | sed 's#^[^[:blank:]]#" .. root .. "/src/&#' | tee"
+      vim.opt.textwidth = 0
     end,
   },
 })
