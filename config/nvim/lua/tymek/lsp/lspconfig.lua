0bbc7a31fafc9cb5132ee8e310bb90edae4aee56
Tymoteusz Makowski <tymek.makowski@gmail.com>
2022-10-17 08:53:47 +0200

    fix(nvim): address deprecation warning

--- config/nvim/lua/tymek/lsp/lspconfig.lua
+++ config/nvim/lua/tymek/lsp/lspconfig.lua
@@ -1,7 +1,7 @@
 local M = {}
 
 local function setup(on_attach)
-  local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
+  local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
   capabilities.textDocument.completion.completionItem.snippetSupport = true
 
   local function config(_config)
