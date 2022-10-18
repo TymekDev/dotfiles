621bf26795a85feffc06bfea7603a4341e01a1f6
Tymoteusz Makowski <tymek.makowski@gmail.com>
2022-10-18 11:02:56 +0200

    feat(nvim): add force push mapping

--- config/nvim/after/ftplugin/fugitive.lua
+++ config/nvim/after/ftplugin/fugitive.lua
@@ -3,3 +3,4 @@ local nnoremap = require("tymek.keymap").nnoremap
 nnoremap("<Leader>gf", { cmd = "Git", args = { "fetch" } })
 nnoremap("<Leader>gl", { cmd = "Git", args = { "pull" } })
 nnoremap("<Leader>gp", { cmd = "Git", args = { "push" } })
+nnoremap("<Leader>gpf", function() vim.notify(vim.fn.system({ "git", "push", "--force-with-lease" })) end)
