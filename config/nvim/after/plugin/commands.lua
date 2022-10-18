694f3ac89cc57f5b7e3ee2c5c2ae04c1ed0548f9
Tymoteusz Makowski <tymek.makowski@gmail.com>
2022-10-18 10:04:29 +0200

    style(nvim): format user command

--- config/nvim/after/plugin/commands.lua
+++ config/nvim/after/plugin/commands.lua
@@ -1,17 +1,15 @@
-vim.api.nvim_create_user_command("Startup", require("telescope.builtin").find_files, {})
+vim.api.nvim_create_user_command("Startup", function()
+  require("telescope.builtin").find_files()
+end, {})
 
 
 local wider_active_buf = vim.opt.winwidth:get() > vim.opt.winwidth._info.default
-vim.api.nvim_create_user_command(
-  "WiderActiveBufToggle",
-  function()
-    if wider_active_buf then
-      vim.opt.winwidth = vim.opt.winwidth._info.default
-      vim.cmd("normal =")
-    else
-      vim.opt.winwidth = 87
-    end
-    wider_active_buf = not wider_active_buf
-  end,
-  {}
-)
+vim.api.nvim_create_user_command("WiderActiveBufToggle", function()
+  if wider_active_buf then
+    vim.opt.winwidth = vim.opt.winwidth._info.default
+    vim.cmd("normal =")
+  else
+    vim.opt.winwidth = 87
+  end
+  wider_active_buf = not wider_active_buf
+end, {})
