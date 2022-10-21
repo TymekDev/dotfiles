f1f453fa513d86474c2e051ab433c55c18a3703c
Tymoteusz Makowski <tymek.makowski@gmail.com>
2022-10-21 14:22:59 +0200

    feat(nvim): start file_browser from file's directory
    This way it behaves more like `:Ex`.

--- config/nvim/plugin/mappings.lua
+++ config/nvim/plugin/mappings.lua
@@ -64,7 +64,7 @@ nnoremap("gh4", function() require("harpoon.ui").nav_file(4) end)
 
 -- Telescope
 nnoremap("<Leader>fq", require("telescope.builtin").quickfix) -- [q]uickfix
-nnoremap("<Leader>fe", require("telescope").extensions.file_browser.file_browser) -- [e]xplore (:Ex)
+nnoremap("<Leader>fe", function() require("telescope").extensions.file_browser.file_browser({ cwd = "%:h" }) end) -- [e]xplore (:Ex)
 nnoremap("<Leader>ff", require("telescope.builtin").find_files) -- [f]iles
 nnoremap("<Leader>fg", require("telescope.builtin").live_grep) -- [g]rep
 nnoremap("<Leader>fh", require("telescope.builtin").help_tags) -- [h]elp
