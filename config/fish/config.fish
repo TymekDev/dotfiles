81d82a7633b5691cc0ebacd356e83af7c4a1c9bf
Tymoteusz Makowski <tymek.makowski@gmail.com>
2022-10-26 08:43:11 +0200

    refactor(fish): remove ~/.tarsnap/bin from  PATH

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -40,7 +40,6 @@ set -x EDITOR nvim
 fish_add_path ~/.cargo/bin
 fish_add_path ~/.local/bin
 fish_add_path ~/.npm-global/bin
-fish_add_path ~/.tarsnap/bin
 fish_add_path ~/go/bin
 fish_add_path /usr/local/go/bin
 
