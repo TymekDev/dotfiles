b0d5f058c11059171b633ca19145bf0ace4461bc
Tymoteusz Makowski <tymek.makowski@gmail.com>
2023-02-12 20:00:30 +0100

    feat(nvim): enable null-ls in .astro files

--- config/nvim/lua/tymek/lsp/null-ls.lua
+++ config/nvim/lua/tymek/lsp/null-ls.lua
@@ -3,6 +3,7 @@ local M = {}
 local function setup(on_attach)
   local prettier = require("null-ls").builtins.formatting.prettier
   prettier.filetypes = vim.tbl_filter(function(x) return x ~= "markdown" end, prettier.filetypes)
+  table.insert(prettier.filetypes, "astro")
 
   require("null-ls").setup({
     on_attach = on_attach,
