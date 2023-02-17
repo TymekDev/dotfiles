b64d6c41658a2a5a84643872a5ad36b4163f7e4d
Tymoteusz Makowski <tymek.makowski@gmail.com>
2023-02-17 12:14:50 +0100

    fix(nvim): make file browser dialog display entries

--- config/nvim/plugin/mappings.lua
+++ config/nvim/plugin/mappings.lua
@@ -95,7 +95,7 @@ nnoremap("gh4", function() require("harpoon.ui").nav_file(4) end)
 -- Telescope
 nnoremap("<Leader>fq", require("telescope.builtin").quickfix) -- [q]uickfix
 -- FIXME: when paths are too long file_browser does not truncate them on picker startup
-nnoremap("<Leader>fe", function() require("telescope").extensions.file_browser.file_browser({ cwd = "%:h" }) end) -- [e]xplore (:Ex)
+nnoremap("<Leader>fe", function() require("telescope").extensions.file_browser.file_browser({ cwd = "%:p:h" }) end) -- [e]xplore (:Ex)
 nnoremap("<Leader>ff", require("telescope.builtin").find_files) -- [f]iles
 nnoremap("<Leader>fg", require("telescope.builtin").live_grep) -- [g]rep
 nnoremap("<Leader>fh", require("telescope.builtin").help_tags) -- [h]elp
