7393f6d15a9a1c38424db403292e0a271c6b30da
Tymoteusz Makowski <tymek.makowski@gmail.com>
2023-03-02 17:11:22 +0100

    refactor(nvim): remove obsolete mapping

--- config/nvim/lua/tymek/lsp/cmp.lua
+++ config/nvim/lua/tymek/lsp/cmp.lua
@@ -16,7 +16,6 @@ cmp.setup({
   mapping = {
     ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
     ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
-    ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
     ["<C-e>"] = cmp.mapping({
       i = cmp.mapping.abort(),
       c = cmp.mapping.close(),
