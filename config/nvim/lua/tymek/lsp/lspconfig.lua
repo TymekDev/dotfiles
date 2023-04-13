6000b5e0745cb09cf2b12c953a54561addc17fec
Tymoteusz Makowski <tymek.makowski@gmail.com>
2023-04-13 22:05:06 +0200

    feat(nvim): add yamlls

--- config/nvim/lua/tymek/lsp/lspconfig.lua
+++ config/nvim/lua/tymek/lsp/lspconfig.lua
@@ -64,6 +64,20 @@ local function setup(on_attach)
   require("lspconfig").tsserver.setup(config({
     filetypes = { "typescript", "typescriptreact", "typescript.tsx" },
   }))
+
+  require("lspconfig").yamlls.setup(config({
+    settings = {
+      yaml = {
+        schemas = {
+          kubernetes = "*.k8s.yaml",
+          ["http://json.schemastore.org/github-workflow"] = ".github/workflows/*",
+          ["http://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
+          ["https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/schemas/v3.1/schema.json"] = "*api*.{yml,yaml}",
+          ["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] = "*docker-compose*.{yml,yaml}",
+        },
+      },
+    },
+  }))
 end
 
 M.setup = setup
