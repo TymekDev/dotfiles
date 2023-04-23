ce70731c1204776590c54cabb07f5ab0b62b1eaa
Tymoteusz Makowski <tymek.makowski@gmail.com>
2023-04-23 21:58:56 +0200

    refactor: move cmp to lazy.nvim

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,2 +1,1 @@
-require("tymek")
 require("tymek.plugins")
--- config/nvim/lua/tymek/plugins.lua
+++ config/nvim/lua/tymek/plugins.lua
@@ -325,6 +325,46 @@ local plugins = {
       })
     end,
   },
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-cmdline",
+      "hrsh7th/cmp-nvim-lsp",
+      {
+        "saadparwaiz1/cmp_luasnip",
+        dependencies = {
+          {
+            "L3MON4D3/LuaSnip",
+            config = function()
+              require("luasnip.loaders.from_vscode").lazy_load()
+            end,
+          },
+        },
+      },
+    },
+    config = function()
+      local cmp = require("cmp")
+
+      cmp.setup.cmdline(":", {
+        sources = cmp.config.sources({
+          { name = "cmdline" },
+        })
+      })
+
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            require("luasnip").lsp_expand(args.body)
+          end,
+        },
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+        }),
+      })
+    end,
+  },
+
 }
 
 require("lazy").setup(plugins, opts)
