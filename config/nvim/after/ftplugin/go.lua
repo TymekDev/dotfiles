00e24fcdddcb1ab918c03f4c05cd1b204146b576
Tymoteusz Makowski <tymek.makowski@gmail.com>
2023-08-25 12:04:08 +0200

    fix: make abbreviations local to buffer

--- config/nvim/after/ftplugin/go.lua
+++ config/nvim/after/ftplugin/go.lua
@@ -2,6 +2,6 @@ vim.opt_local.expandtab = false
 vim.opt_local.shiftwidth = 4
 vim.opt_local.tabstop = 4
 
-vim.cmd.iabbrev("iferr if err")
-vim.cmd.iabbrev("returnn return n")
-vim.cmd.iabbrev("returne return e")
+vim.cmd.iabbrev("<buffer> iferr if err")
+vim.cmd.iabbrev("<buffer> returnn return n")
+vim.cmd.iabbrev("<buffer> returne return e")
