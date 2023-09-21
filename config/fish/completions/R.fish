1a8b0892930f28cfb974101e692c990915383f3d
Tymoteusz Makowski <tymek.makowski@gmail.com>
2023-09-21 11:13:23 +0200

    feat: add R completions

---
+++ config/fish/completions/R.fish
@@ -0,0 +1,9 @@
+complete -c R -f
+complete -c R -l help
+complete -c R -l no-environ
+complete -c R -l no-init-file
+complete -c R -l no-save
+complete -c R -l no-site-file
+complete -c R -l vanilla
+complete -c R -l version
+complete -c R -o f -l file -r
