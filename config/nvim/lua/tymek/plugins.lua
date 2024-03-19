123a61da7c32a1fdf3f174e52aae43e62965141b
Tymoteusz Makowski <tymek.makowski@gmail.com>
2024-03-19 11:16:49 +0100

    fix: add missing dependency to nvim-dap-ui

--- config/nvim/lua/tymek/plugins.lua
+++ config/nvim/lua/tymek/plugins.lua
@@ -446,7 +446,7 @@ return {
   -- },
   {
     "rcarriga/nvim-dap-ui",
-    dependencies = { "mfussenegger/nvim-dap" },
+    dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
     config = function()
       require("dapui").setup()
       require("dap").listeners.after.event_initialized["dapui_config"] = require("dapui").open
