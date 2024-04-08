bf096f6d6e8e1af8cee96070f506be27f92d663f
Tymoteusz Makowski <tymek.makowski@gmail.com>
2024-04-08 11:44:55 +0200

    refactor: remove c from shortmess

--- config/nvim/plugin/options.lua
+++ config/nvim/plugin/options.lua
@@ -13,7 +13,7 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.scrolloff = 3
 vim.opt.shiftwidth = 2
-vim.opt.shortmess:append({ c = true, I = true })
+vim.opt.shortmess:append({ I = true })
 vim.opt.sidescrolloff = 5
 vim.opt.signcolumn = "yes"
 vim.opt.smartindent = true
