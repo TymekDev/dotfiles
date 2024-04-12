093eabf949d0296f8a621c71c4ef7509e2a5b10a
Tymoteusz Makowski <tymek.makowski@gmail.com>
2024-04-12 15:42:22 +0200

    fix: update Neorg configuration

--- config/nvim/lua/tymek/plugins.lua
+++ config/nvim/lua/tymek/plugins.lua
@@ -482,8 +482,12 @@ return {
   },
   {
     "nvim-neorg/neorg",
-    build = ":Neorg sync-parsers",
-    dependencies = { "nvim-lua/plenary.nvim" },
+    dependencies = {
+      {
+        "vhyrro/luarocks.nvim",
+        config = true,
+      }
+    },
     config = function()
       require("neorg").setup({
         load = {
