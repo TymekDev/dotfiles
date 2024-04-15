1d3c2db0e7edfa0411a911fff3ce82974c5ad1f5
Tymoteusz Makowski <tymek.makowski@gmail.com>
2024-04-15 09:17:43 +0200

    fix: update highlight group name

--- config/nvim/after/ftplugin/norg.lua
+++ config/nvim/after/ftplugin/norg.lua
@@ -1,5 +1,5 @@
 vim.opt_local.spell = true
 
-vim.api.nvim_set_hl(0, "@neorg.markup.italic", { italic = true })
+vim.api.nvim_set_hl(0, "@neorg.markup.italic.norg", { italic = true })
 
 vim.keymap.set("n", "<Leader>C", "<Cmd>Neorg toggle-concealer<CR>", { buffer = 0 })
