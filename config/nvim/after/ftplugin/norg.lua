edb5704270c3a7544c73f597fa0688d1279d3ca2
Tymoteusz Makowski <tymek.makowski@gmail.com>
2024-04-18 10:41:35 +0200

    feat(neorg): update metadata on write

--- config/nvim/after/ftplugin/norg.lua
+++ config/nvim/after/ftplugin/norg.lua
@@ -3,3 +3,8 @@ vim.opt_local.spell = true
 vim.api.nvim_set_hl(0, "@neorg.markup.italic.norg", { italic = true })
 
 vim.keymap.set("n", "<Leader>C", "<Cmd>Neorg toggle-concealer<CR>", { buffer = 0 })
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+  buffer = 0,
+  command = "Neorg update-metadata",
+})
