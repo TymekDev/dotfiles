ad2113524dfba90acd3c3f1b1fa804edc9288229
Tymoteusz Makowski <tymek.makowski@gmail.com>
2024-06-06 18:13:37 +0200

    fix: increase timeout to cater to R :-/

--- config/nvim/lua/tymek/plugins/conform.lua
+++ config/nvim/lua/tymek/plugins/conform.lua
@@ -10,7 +10,7 @@ return {
       if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
         return
       end
-      return { timeout_ms = 500, lsp_fallback = true }
+      return { timeout_ms = 2000, lsp_fallback = true }
     end,
     formatters_by_ft = {
       astro = {
