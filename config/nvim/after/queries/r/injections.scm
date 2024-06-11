a2028b0cbec3f100adf234ba72aa7cfa45e8b33f
Tymoteusz Makowski <tymek.makowski@gmail.com>
2024-06-11 09:06:24 +0200

    feat: add an injection for glue_sql

--- config/nvim/after/queries/r/injections.scm
+++ config/nvim/after/queries/r/injections.scm
@@ -13,3 +13,12 @@
     (#set! injection.language "javascript")
   )
 )
+
+(call
+  function: (identifier) @function (#eq? @function "glue_sql")
+  arguments: (arguments
+    (string) @injection.content
+    (#offset! @injection.content 0 1 0 -1)
+    (#set! injection.language "sql")
+  )
+)
