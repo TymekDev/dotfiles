11d9eb106ada00c01f9211566bcc874d29ad07df
Tymoteusz Makowski <tymek.makowski@gmail.com>
2024-06-21 10:40:49 +0200

    refactor: move gitsigns.nvim mappings to on_attach

--- config/nvim/lua/tymek/plugins/gitsigns.lua
+++ config/nvim/lua/tymek/plugins/gitsigns.lua
@@ -1,9 +1,20 @@
 return {
   "lewis6991/gitsigns.nvim", -- TODO: review config
   event = "VeryLazy",
-  keys = {
-    { "[g", "<Cmd>Gitsigns prev_hunk<CR>", desc = "Jump to the previous git hunk (via gitsigns.nvim)" },
-    { "]g", "<Cmd>Gitsigns next_hunk<CR>", desc = "Jump to the next git hunk (via gitsigns.nvim)" },
+  opts = {
+    on_attach = function()
+      vim.keymap.set(
+        { "n", "v" },
+        "[g",
+        "<Cmd>Gitsigns prev_hunk<CR>",
+        { desc = "Jump to the previous git hunk (via gitsigns.nvim)" }
+      )
+      vim.keymap.set(
+        { "n", "v" },
+        "]g",
+        "<Cmd>Gitsigns next_hunk<CR>",
+        { desc = "Jump to the next git hunk (via gitsigns.nvim)" }
+      )
+    end,
   },
-  config = true,
 }
