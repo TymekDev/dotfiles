04f14dd6301f9a15bf6a5286bb780a4bf6d27e06
Tymoteusz Makowski <tymek.makowski@gmail.com>
2024-07-08 22:36:23 +0200

    refactor: make nvim-surround lazy load on mappings

--- config/nvim/lua/tymek/plugins/nvim-surround.lua
+++ config/nvim/lua/tymek/plugins/nvim-surround.lua
@@ -1,6 +1,20 @@
+---@module "lazy"
+---@type LazySpec
 return {
   "kylechui/nvim-surround",
-  event = "VeryLazy",
+  keys = {
+    -- stylua: ignore start
+    { "ys", "<Plug>(nvim-surround-normal)", desc = "Surround a motion (via nvim-surround)" },
+    { "yss", "<Plug>(nvim-surround-normal-cur)", desc = "Surround the current line (via nvim-surround)" },
+    { "yS", "<Plug>(nvim-surround-normal-line)", desc = "Surround a motion, place surrounding on new lines (via nvim-surround)" },
+    { "ySS", "<Plug>(nvim-surround-normal-cur-line)", desc = "Surround the current line, place surrounding on new lines (via nvim-surround)" },
+    { "S", "<Plug>(nvim-surround-visual)", desc = "Surround the selection (via nvim-surround)" },
+    { "gS", "<Plug>(nvim-surround-visual-line)", desc = "Surround the selection, place surrounding on new lines (via nvim-surround)" },
+    { "ds", "<Plug>(nvim-surround-delete)", desc = "Delete the surrounding (via nvim-surround)" },
+    { "cs", "<Plug>(nvim-surround-change)", desc = "Change the surrounding (via nvim-surround)" },
+    { "cS", "<Plug>(nvim-surround-change-line)", desc = "Change the surrounding (via nvim-surround)" },
+    -- stylua: ignore stop
+  },
   ---@module "nvim-surround"
   ---@type user_options
   opts = {
