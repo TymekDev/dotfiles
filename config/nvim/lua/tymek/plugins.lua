e596e5225e62ab4a07b73eccd7484ae3acd02f9e
Tymoteusz Makowski <tymek.makowski@gmail.com>
2024-07-31 13:36:01 +0200

    refactor: move cmp spec into cmp.lua

--- config/nvim/lua/tymek/plugins.lua
+++ config/nvim/lua/tymek/plugins.lua
@@ -237,55 +237,6 @@ return {
     end,
   },
   {
-    "hrsh7th/nvim-cmp",
-    dependencies = {
-      "hrsh7th/cmp-nvim-lsp-signature-help",
-      "hrsh7th/cmp-nvim-lsp",
-      {
-        "saadparwaiz1/cmp_luasnip",
-        dependencies = {
-          {
-            "L3MON4D3/LuaSnip",
-            config = function()
-              require("luasnip.loaders.from_vscode").lazy_load()
-            end,
-          },
-        },
-      },
-      "hrsh7th/cmp-path",
-    },
-    config = function()
-      require("cmp").setup({
-        snippet = {
-          expand = function(args)
-            require("luasnip").lsp_expand(args.body)
-          end,
-        },
-        sources = require("cmp").config.sources({
-          { name = "nvim_lsp_signature_help" },
-          { name = "nvim_lsp" },
-          { name = "cmp_r" },
-          { name = "luasnip" },
-          {
-            name = "path",
-            option = { trailing_slash = true },
-          },
-        }),
-      })
-    end,
-  },
-  {
-    "hrsh7th/cmp-cmdline",
-    dependencies = { "hrsh7th/nvim-cmp" },
-    config = function()
-      require("cmp").setup.cmdline(":", {
-        sources = require("cmp").config.sources({
-          { name = "cmdline" },
-        }),
-      })
-    end,
-  },
-  {
     "rcarriga/nvim-dap-ui",
     dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
     config = function()
