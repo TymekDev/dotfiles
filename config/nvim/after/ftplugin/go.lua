a49634e5a44e555a80a811c6cd24bf4dff4c1b94
Tymoteusz Makowski <tymek.makowski@gmail.com>
2024-08-05 09:10:03 +0200

    refactor: remove abbreviations

--- config/nvim/after/ftplugin/go.lua
+++ config/nvim/after/ftplugin/go.lua
@@ -1,7 +1,3 @@
 vim.opt_local.expandtab = false
 vim.opt_local.shiftwidth = 4
 vim.opt_local.tabstop = 4
-
-vim.cmd.iabbrev("<buffer> iferr if err")
-vim.cmd.iabbrev("<buffer> returnnil return nil")
-vim.cmd.iabbrev("<buffer> returnerr return err")
