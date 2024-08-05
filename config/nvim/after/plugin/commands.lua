f6667438eced229df2c96513f09430b7acabc877
Tymoteusz Makowski <tymek.makowski@gmail.com>
2024-08-05 15:48:10 +0200

    refactor: use vim.api instead of vim.opt

--- config/nvim/after/plugin/commands.lua
+++ config/nvim/after/plugin/commands.lua
@@ -1,7 +1,8 @@
-local wider_active_buf = vim.opt.winwidth:get() > vim.opt.winwidth._info.default
+local wider_active_buf = vim.api.nvim_get_option_value("winwidth", {})
+  > vim.api.nvim_get_option_info2("winwidth", {}).default
 vim.api.nvim_create_user_command("WiderActiveBufToggle", function()
   if wider_active_buf then
-    vim.opt.winwidth = vim.opt.winwidth._info.default
+    vim.opt.winwidth = vim.api.nvim_get_option_info2("winwidth", {}).default
     vim.cmd("normal =")
   else
     vim.opt.winwidth = 87
