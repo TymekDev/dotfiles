0c97c2f9749eeb7a6a92d3ec88591bb8870fc159
Tymoteusz Makowski <tymek.makowski@gmail.com>
2024-08-08 08:36:15 +0200

    feat: configure quicker.nvim

---
+++ config/nvim/lua/tymek/plugins/quicker.lua
@@ -0,0 +1,61 @@
+---@module "lazy"
+---@type LazySpec
+return {
+  "stevearc/quicker.nvim",
+  event = "QuickFixCmdPost",
+  ---@module "quicker"
+  ---@type quicker.SetupOptions
+  opts = {
+    max_filename_width = function()
+      -- TODO: parametrize this and add a mapping to increase this value
+      return 30
+    end,
+    keys = {
+      {
+        ">",
+        function()
+          require("quicker").expand({ before = 2, after = 2, add_to_existing = true })
+        end,
+        desc = "Expand quickfix context (via quicker.nvim)",
+      },
+      {
+        "<",
+        function()
+          require("quicker").collapse()
+        end,
+        desc = "Collapse quickfix context (via quicker.nvim)",
+      },
+      -- FEAT: make this wrap
+      {
+        "]q",
+        function()
+          local items = vim.fn.getqflist()
+          local lnum = vim.api.nvim_win_get_cursor(0)[1]
+          for i = lnum + 1, #items do
+            if items[i].valid == 1 then
+              vim.api.nvim_win_set_cursor(0, { i, 0 })
+              return
+            end
+          end
+        end,
+        desc = "Go to the next entry without jumping to its location (via quicker.nvim)",
+      },
+      {
+        "[q",
+        function()
+          local items = vim.fn.getqflist()
+          local lnum = vim.api.nvim_win_get_cursor(0)[1]
+          for i = lnum - 1, 1, -1 do
+            if items[i].valid == 1 then
+              vim.api.nvim_win_set_cursor(0, { i, 0 })
+              return
+            end
+          end
+        end,
+        desc = "Go to the previous entry without jumping to its location (via quicker.nvim)",
+      },
+    },
+    trim_leading_whitespace = false,
+    use_default_opts = false,
+  },
+}
