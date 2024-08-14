7832850f7bd9d5e3d6ce9e69607b1b8023c3757b
Tymoteusz Makowski <tymek.makowski@gmail.com>
2024-08-14 12:28:03 +0200

    refactor: remove an unnecessary autocmd
    I _think_ quicker.nvim handles this.

--- config/nvim/after/plugin/autocmds.lua
+++ config/nvim/after/plugin/autocmds.lua
@@ -1,9 +1,3 @@
-vim.api.nvim_create_autocmd("QuickfixCmdPost", {
-  callback = function()
-    vim.api.nvim_cmd({ cmd = "cwindow" }, {})
-  end,
-})
-
 vim.api.nvim_create_autocmd("TextYankPost", {
   callback = function()
     vim.highlight.on_yank({
