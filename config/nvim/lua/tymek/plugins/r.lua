5a8e41648aa22c24d0d139925fea883e2a2446f0
Tymoteusz Makowski <tymek.makowski@gmail.com>
2024-09-17 10:06:52 +0200

    style: reduce nesting

--- config/nvim/lua/tymek/plugins/r.lua
+++ config/nvim/lua/tymek/plugins/r.lua
@@ -5,26 +5,24 @@ local commands = {
 ---@module "lazy"
 ---@type LazySpec
 return {
-  {
-    "R-nvim/R.nvim",
-    ft = "r",
-    keys = {
-      {
-        "<Leader>re",
-        function()
-          vim.ui.select(commands, { prompt = "Execute in R" }, function(cmd)
-            if cmd ~= nil then
-              vim.cmd.RSend(cmd)
-            end
-          end)
-        end,
-        ft = "r",
-        desc = "Execute one of a predefined commands in R (via R.nvim)",
-      },
+  "R-nvim/R.nvim",
+  ft = "r",
+  keys = {
+    {
+      "<Leader>re",
+      function()
+        vim.ui.select(commands, { prompt = "Execute in R" }, function(cmd)
+          if cmd ~= nil then
+            vim.cmd.RSend(cmd)
+          end
+        end)
+      end,
+      ft = "r",
+      desc = "Execute one of a predefined commands in R (via R.nvim)",
     },
-    opts = {
-      setwd = "nvim",
-      hl_term = false,
-    },
+  },
+  opts = {
+    setwd = "nvim",
+    hl_term = false,
   },
 }
