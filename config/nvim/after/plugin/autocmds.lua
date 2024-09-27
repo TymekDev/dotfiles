f7f48979da6025dbe98d5040facdb04f35e2b6ee
Tymoteusz Makowski <tymek.makowski@gmail.com>
2024-09-27 10:05:10 +0200

    feat: add do and dp mappings for visual selection

--- config/nvim/after/plugin/autocmds.lua
+++ config/nvim/after/plugin/autocmds.lua
@@ -10,3 +10,17 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 vim.api.nvim_create_autocmd("FocusGained", {
   callback = require("tymek.theme").update,
 })
+
+vim.api.nvim_create_autocmd("OptionSet", {
+  pattern = "diff",
+  callback = function()
+    vim.keymap.set("v", "do", ":diffget<CR>", {
+      buffer = 0,
+      desc = "Diff obtain the selected range",
+    })
+    vim.keymap.set("v", "dp", ":diffput<CR>", {
+      buffer = 0,
+      desc = "Diff put the selected range",
+    })
+  end,
+})
