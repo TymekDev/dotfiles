82c2930b3567ca69bc6feafb3f44b05a7c0a9dac
Tymoteusz Makowski <tymek.makowski@gmail.com>
2024-10-10 22:46:56 +0200

    feat: add on-demand formatting

--- config/nvim/lua/tymek/plugins/conform.lua
+++ config/nvim/lua/tymek/plugins/conform.lua
@@ -23,6 +23,12 @@ return {
   "stevearc/conform.nvim",
   event = "VeryLazy",
   keys = {
+    {
+      mode = "v",
+      "<Leader>f",
+      ":FormatLines<CR>",
+      desc = "Format the selected lines asynchronously (via conform.nvim)",
+    },
     { "<Leader>F", "<Cmd>FormatToggle<CR>", desc = "Toggle autoformat-on-save (via conform.nvim)" },
   },
   ---@module "conform"
@@ -56,6 +62,17 @@ return {
     },
   },
   init = function()
+    vim.api.nvim_create_user_command("FormatLines", function(args)
+      local range = nil
+      if args.count ~= -1 then
+        local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
+        range = {
+          start = { args.line1, 0 },
+          ["end"] = { args.line2, end_line:len() },
+        }
+      end
+      require("conform").format({ async = true, lsp_format = "fallback", range = range })
+    end, { range = true })
     vim.api.nvim_create_user_command("FormatToggle", function(opts)
       if opts.bang then
         vim.g.disable_autoformat = not vim.g.disable_autoformat or false
