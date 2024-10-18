dbbf7e1359d3d6c5304d60d9b310f4f2e6320879
Tymoteusz Makowski <tymek.makowski@gmail.com>
2024-10-18 20:11:27 +0200

    feat: add <Leader>c for opening a new tmux window in the current oil.nvim directory

--- config/nvim/lua/tymek/plugins/oil.lua
+++ config/nvim/lua/tymek/plugins/oil.lua
@@ -6,6 +6,15 @@ return {
   dependencies = { "nvim-tree/nvim-web-devicons" },
   keys = {
     { "-", "<Cmd>Oil<CR>", desc = "Open the parent directory (via oil.nvim)" },
+    {
+      "<Leader>c",
+      function()
+        vim.system({ "tmux", "new-window", "-c", require("oil").get_current_dir(0) })
+      end,
+      ft = "oil",
+      nowait = true,
+      desc = "Open the current directory in a new tmux window (via oil.nvim)",
+    },
   },
   ---@module "oil"
   ---@type oil.setupOpts
