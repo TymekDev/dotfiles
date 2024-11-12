005aad833cc6781f284c965e129e32d7995bf004
Tymoteusz Makowski <tymek.makowski@gmail.com>
2024-11-12 07:52:18 +0100

    feat: add avante.nvim

---
+++ config/nvim/lua/tymek/plugins/avante.lua
@@ -0,0 +1,27 @@
+---@module "lazy"
+---@type LazySpec
+return {
+  {
+    "yetone/avante.nvim",
+    event = "VeryLazy",
+    build = "make",
+    ---@module "avante"
+    ---@type avante.Config
+    opts = {
+      -- provider = "openai",
+      -- auto_suggestions_provider = "openai",
+    },
+  },
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    ft = "Avante",
+    opts = {
+      file_types = { "Avante" },
+    },
+  },
+  "nvim-treesitter/nvim-treesitter",
+  "stevearc/dressing.nvim",
+  "nvim-lua/plenary.nvim",
+  "MunifTanjim/nui.nvim",
+  "nvim-tree/nvim-web-devicons",
+}
