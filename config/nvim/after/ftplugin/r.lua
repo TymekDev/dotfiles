9b886c24ac0e07f03721bc3f518ba753e4fa015d
Tymoteusz Makowski <tymek.makowski@gmail.com>
2024-11-15 15:55:17 +0100

    feat(r): add insert-mode mappings (insert '<-' and append '|>')

--- config/nvim/after/ftplugin/r.lua
+++ config/nvim/after/ftplugin/r.lua
@@ -1,3 +1,4 @@
+-- vim:foldenable
 local queries = require("tymek.treesitter.queries.r")
 local ts = require("tymek.treesitter")
 local ts_r = require("tymek.treesitter.r")
@@ -5,7 +6,18 @@ local ts_r = require("tymek.treesitter.r")
 vim.g.r_indent_align_args = false
 vim.opt_local.textwidth = 100
 
+vim.keymap.set({ "n", "i" }, "<M-p>", function()
+  local bufnr = 0
+  local line_index = vim.api.nvim_win_get_cursor(bufnr)[1]
+  local line = vim.api.nvim_buf_get_lines(bufnr, line_index - 1, line_index, true)[1] .. " |>"
+  vim.api.nvim_buf_set_lines(bufnr, line_index - 1, line_index, true, { line })
+end, {
+  buffer = 0,
+  desc = "Append a native pipe to the end of the current line",
+})
+vim.keymap.set("i", "<M-,>", " <- ")
 vim.keymap.set("n", "<Leader>bi", ts_r.put_missing_box_imports, { buffer = 0 })
+
 vim.api.nvim_create_autocmd({ "BufWinEnter", "TextChanged", "TextChangedI" }, {
   desc = "Highlight reactives and eventReactives declared in the current buffer",
   buffer = 0,
