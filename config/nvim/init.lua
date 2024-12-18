2987c55f3117c17cc588d43e8cbf3ced37ef68a6
Tymoteusz Makowski <tymek.makowski@gmail.com>
2024-12-18 22:27:13 +0100

    refactor: move lazy.nvim setup to lazy.lua

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,51 +1,5 @@
--- lazy.nvim bootstrap start
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.uv.fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
-end
-vim.opt.rtp:prepend(lazypath)
--- lazy.nvim bootstrap end
-
 vim.g.mapleader = " "
-require("lazy").setup("tymek.plugins", {
-  dev = {
-    path = "~/personal",
-  },
-  install = {
-    colorscheme = { "tokyonight" },
-  },
-  performance = {
-    rtp = {
-      disabled_plugins = {
-        "editorconfig",
-        "gzip",
-        "health",
-        "man",
-        -- "matchit",
-        -- "matchparen",
-        "netrwPlugin",
-        "nvim",
-        "rplugin",
-        -- "shada",
-        -- "spellfile",
-        "tarPlugin",
-        "tohtml",
-        "tutor",
-        "zipPlugin",
-      },
-    },
-  },
-})
+require("tymek.lazy").setup()
 
 vim.api.nvim_cmd({
   cmd = "packadd",
