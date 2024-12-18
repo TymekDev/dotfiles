559d18969d86a1da644303c60d581a73bbc39594
Tymoteusz Makowski <tymek.makowski@gmail.com>
2024-12-18 12:33:39 +0100

    feat: add snacks.nvim and configure debug

---
+++ config/nvim/lua/tymek/plugins/snacks.lua
@@ -0,0 +1,24 @@
+---@module "lazy"
+---@type LazySpec
+return {
+  "folke/snacks.nvim",
+  lazy = false,
+  ---@module "snacks"
+  ---@type snacks.Config
+  opts = {},
+  init = function()
+    vim.api.nvim_create_autocmd("User", {
+      pattern = "VeryLazy",
+      callback = function()
+        -- Setup some globals for debugging (lazy-loaded)
+        _G.dd = function(...)
+          Snacks.debug.inspect(...)
+        end
+        _G.bt = function()
+          Snacks.debug.backtrace()
+        end
+        vim.print = _G.dd -- Override print to use snacks for `:=` command
+      end,
+    })
+  end,
+}
