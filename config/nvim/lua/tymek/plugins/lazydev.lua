96cdb0e047c9e488123ff404982af6e2c46a81d4
Tymoteusz Makowski <tymek.makowski@gmail.com>
2024-12-19 09:46:48 +0100

    refactor: update lazydev.nvim spec

--- config/nvim/lua/tymek/plugins/lazydev.lua
+++ config/nvim/lua/tymek/plugins/lazydev.lua
@@ -1,19 +1,18 @@
 ---@module "lazy"
 ---@type LazySpec
 return {
-  {
-    "folke/lazydev.nvim",
-    ft = "lua",
-    ---@module "lazydev"
-    ---@type lazydev.Config
-    opts = {
-      library = {
-        { path = "luvit-meta/library", words = { "vim%.uv" } },
-        { path = "wezterm-types", mods = { "wezterm" } },
-      },
+  "folke/lazydev.nvim",
+  ft = "lua",
+  dependencies = {
+    { "Bilal2453/luvit-meta" }, -- vim.uv
+    { "justinsgithub/wezterm-types" }, -- wezterm
+  },
+  ---@module "lazydev"
+  ---@type lazydev.Config
+  opts = {
+    library = {
+      { path = "luvit-meta/library", words = { "vim%.uv" } },
+      { path = "wezterm-types", mods = { "wezterm" } },
     },
   },
-  { "Bilal2453/luvit-meta", lazy = true }, -- vim.uv
-  -- FIXME: wezterm types don't really work
-  { "justinsgithub/wezterm-types", lazy = true }, -- wezterm
 }
