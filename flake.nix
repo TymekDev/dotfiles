3486abe4ba674ca7ec3c457b1ee1029f00a4e47d
Tymoteusz Makowski <tymek.makowski@gmail.com>
2025-05-06 22:14:12 +0200

    feat: add flake.nix

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "TymekDev's NixOS flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+
+    disko = {
+      url = "github:nix-community/disko";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    { self, nixpkgs, disko, ... } @ inputs:
+    {
+      nixosConfigurations = {
+        sffpc = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            disko.nixosModules.disko
+            ./disko.nix
+            {
+              boot.loader.systemd-boot.enable = true;
+              boot.loader.efi.canTouchEfiVariables = true;
+              system.stateVersion = "24.11";
+            }
+          ];
+        };
+      };
+    };
+}
