b4ef99ebdc51d1917dacc0b3ad21881fa0fb510c
Tymoteusz Makowski <tymek.makowski@gmail.com>
2025-05-07 21:47:38 +0200

    feat: add user

--- flake.nix
+++ flake.nix
@@ -23,6 +23,11 @@
               boot.loader.systemd-boot.enable = true;
               boot.loader.efi.canTouchEfiVariables = true;
               system.stateVersion = "24.11";
+              users.users.tymek = {
+                isNormalUser = true;
+                home = "/home/tymek";
+                extraGroups = [ "wheel" "networkmanager" ];
+              };
             }
           ];
         };
