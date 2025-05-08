5173e10c41c7c60fb6be84f2c563059ddf334c2f
Tymoteusz Makowski <tymek.makowski@gmail.com>
2025-05-08 08:07:33 +0200

    refactor: make <leader>r start Rabbit trail

--- config/nvim/lua/tymek/plugins/rabbit.lua
+++ config/nvim/lua/tymek/plugins/rabbit.lua
@@ -2,8 +2,16 @@
 ---@type LazySpec
 return {
   "voxelprismatic/rabbit.nvim",
+  lazy = false,
+  keys = {
+    { "<Leader>r", "<Cmd>Rabbit trail<CR>", desc = "Browse previously visited buffers (via rabbit.nvim)" },
+  },
   branch = "rewrite",
   ---@module "rabbit"
   ---@type Rabbit.Config
-  opts = {},
+  opts = {
+    keys = {
+      switch = {},
+    },
+  },
 }
