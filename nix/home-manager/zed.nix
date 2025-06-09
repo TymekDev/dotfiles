f29fc2df2d4593adf69452e48b01795995137ed8
Tymoteusz Makowski <tymek.makowski@gmail.com>
2025-06-09 22:34:43 +0200

    refactor: switch from $HOME to ~

--- nix/home-manager/zed.nix
+++ nix/home-manager/zed.nix
@@ -3,6 +3,6 @@
   home.packages = [ pkgs-unstable.zed-editor ];
 
   home.activation.symlinkZed = lib.hm.dag.entryAfter ["writeBoundary"] ''
-    run [ ! -L $HOME/.config/zed ] && ln -s $VERBOSE_ARG $HOME/personal/dotfiles/config/zed/ -t $HOME/.config/ || exit 0
+    run [ ! -L ~/.config/zed ] && ln -s $VERBOSE_ARG $HOME/personal/dotfiles/config/zed/ -t $HOME/.config/ || exit 0
   '';
 }
