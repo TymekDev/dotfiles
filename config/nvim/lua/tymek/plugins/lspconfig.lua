03611a50a2db28b819f05980988e8bc5eb24e9b3
Tymoteusz Makowski <tymek.makowski@gmail.com>
2025-06-28 21:06:24 +0200

    refactor: include more taskfile name variants

--- config/nvim/lua/tymek/plugins/lspconfig.lua
+++ config/nvim/lua/tymek/plugins/lspconfig.lua
@@ -169,7 +169,7 @@ languageserver::run()
                 ["http://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
                 ["https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/schemas/v3.1/schema.json"] = "*api*.{yml,yaml}",
                 ["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] = "*docker-compose*.{yml,yaml}",
-                ["https://taskfile.dev/schema.json"] = "Taskfile.{yml,yaml}",
+                ["https://taskfile.dev/schema.json"] = "{t,T}askfile.{yml,yaml}",
               },
             },
             redhat = {
