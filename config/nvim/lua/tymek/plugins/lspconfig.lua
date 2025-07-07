c864123d5be9235002869bd4babff9fdde376e4e
Tymoteusz Makowski <tymek.makowski@gmail.com>
2025-07-07 10:34:58 +0200

    refactor: use schemastore plugin

--- config/nvim/lua/tymek/plugins/lspconfig.lua
+++ config/nvim/lua/tymek/plugins/lspconfig.lua
@@ -162,14 +162,8 @@ languageserver::run()
         yamlls = {
           settings = {
             yaml = {
-              schemas = {
-                kubernetes = "*.k8s.yaml",
-                ["http://json.schemastore.org/github-workflow"] = ".github/workflows/*",
-                ["http://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
-                ["https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/schemas/v3.1/schema.json"] = "*api*.{yml,yaml}",
-                ["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] = "*docker-compose*.{yml,yaml}",
-                ["https://taskfile.dev/schema.json"] = "{t,T}askfile.{yml,yaml}",
-              },
+              schemaStore = { enable = false, url = "" },
+              schemas = require("schemastore").yaml.schemas(),
             },
             redhat = {
               telemetry = {
