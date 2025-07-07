0f007aa66eae8ea33d63760666af95a1ed6ef48f
Tymoteusz Makowski <tymek.makowski@gmail.com>
2025-07-07 12:50:47 +0200

    refactor: drop v-mode mappings where they don't matter

--- config/nvim/lua/tymek/mappings.lua
+++ config/nvim/lua/tymek/mappings.lua
@@ -12,10 +12,10 @@ local M = {}
 
 M.setup = function()
   -- Splits
-  vim.keymap.set({ "n", "v" }, "gh", "<C-w>h")
-  vim.keymap.set({ "n", "v" }, "gj", "<C-w>j")
-  vim.keymap.set({ "n", "v" }, "gk", "<C-w>k")
-  vim.keymap.set({ "n", "v" }, "gl", "<C-w>l")
+  vim.keymap.set("n", "gh", "<C-w>h")
+  vim.keymap.set("n", "gj", "<C-w>j")
+  vim.keymap.set("n", "gk", "<C-w>k")
+  vim.keymap.set("n", "gl", "<C-w>l")
 
   -- Screen
   vim.keymap.set({ "n", "v" }, "<C-u>", "<C-u>zz") -- Half a screen up with center on cursor
@@ -27,25 +27,25 @@ M.setup = function()
   vim.keymap.set({ "n", "v" }, "N", "Nzzzv") -- Previous search result, center on cursor, and open folds
 
   -- Quickfix list
-  vim.keymap.set({ "n", "v" }, "<C-h>", function() -- Previous quickfix list with opening if non-empty
+  vim.keymap.set("n", "<C-h>", function() -- Previous quickfix list with opening if non-empty
     vim.cmd.colder({ count = vim.api.nvim_eval("v:count1") })
     vim.cmd.cwindow()
   end)
-  vim.keymap.set({ "n", "v" }, "<C-j>", function() -- Next quickfix entry with wrapping and opening if non-empty
+  vim.keymap.set("n", "<C-j>", function() -- Next quickfix entry with wrapping and opening if non-empty
     local ok, msg = pcall(vim.cmd.cnext, { count = vim.api.nvim_eval("v:count1") })
     if ok == false and msg == "Vim:E553: No more items" then
       vim.cmd.cfirst()
     end
     vim.cmd.cwindow()
   end)
-  vim.keymap.set({ "n", "v" }, "<C-k>", function() -- Previous quickfix entry with wrapping and opening if non-empty
+  vim.keymap.set("n", "<C-k>", function() -- Previous quickfix entry with wrapping and opening if non-empty
     local ok, msg = pcall(vim.cmd.cprev, { count = vim.api.nvim_eval("v:count1") })
     if ok == false and msg == "Vim:E553: No more items" then
       vim.cmd.clast()
     end
     vim.cmd.cwindow()
   end)
-  vim.keymap.set({ "n", "v" }, "<C-l>", function() -- Next quickfix list with opening if non-empty
+  vim.keymap.set("n", "<C-l>", function() -- Next quickfix list with opening if non-empty
     vim.cmd.cnewer({ count = vim.api.nvim_eval("v:count1") })
     vim.cmd.cwindow()
   end)
@@ -56,7 +56,7 @@ M.setup = function()
   vim.keymap.set({ "n", "v" }, "<Leader>Y", '"+Y')
 
   -- Miscellaneous
-  vim.keymap.set({ "n", "v" }, "<C-s>", "<Cmd>silent !tmux run-shell tmux-sessionizer<CR>")
+  vim.keymap.set("n", "<C-s>", "<Cmd>silent !tmux run-shell tmux-sessionizer<CR>")
   vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]])
 end
 
