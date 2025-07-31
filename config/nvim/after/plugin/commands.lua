466018b2e839bc0293fb3136ae51c479f3521179
Tymoteusz Makowski <tymek.makowski@gmail.com>
2025-07-31 12:45:13 +0200

    feat: add RLintRhino command

--- config/nvim/after/plugin/commands.lua
+++ config/nvim/after/plugin/commands.lua
@@ -103,12 +103,12 @@ vim.api.nvim_create_user_command(
   }
 )
 
-vim.api.nvim_create_user_command("RLintPackage", function()
+local run_lintr = function(code)
   vim.system(
     {
       "Rscript",
       "-e",
-      "lintr::lint_package()",
+      code,
     },
     {},
     vim.schedule_wrap(function(result)
@@ -122,6 +122,16 @@ vim.api.nvim_create_user_command("RLintPackage", function()
       vim.cmd.copen()
     end)
   )
+end
+
+vim.api.nvim_create_user_command("RLintPackage", function()
+  run_lintr("lintr::lint_package()")
 end, {
   desc = "Run lintr::lint_package() and populate the quickfix list with the results",
+})
+
+vim.api.nvim_create_user_command("RLintRhino", function()
+  run_lintr("rhino::lint_r()")
+end, {
+  desc = "Run rhino::lint_r() and populate the quickfix list with the results",
 })
