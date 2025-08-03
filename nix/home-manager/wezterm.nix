c0d7ba487e3bd6c5ea3e25a30f32f63f962a259d
Tymoteusz Makowski <tymek.makowski@gmail.com>
2025-08-03 21:25:47 +0200

    fix(nixos): account for updates to Wezterm theme handling

--- nix/home-manager/wezterm.nix
+++ nix/home-manager/wezterm.nix
@@ -1,6 +1,10 @@
-{ ... } :
+{ lib, ... } :
 {
   xdg.configFile."wezterm/theme.lua".source = ../../config/wezterm/theme.lua;
+
+  home.activation.weztermMkdirThemeDir = lib.hm.dag.entryAfter ["writeBoundary"] ''
+    run mkdir $VERBOSE_ARG --parents $HOME/.local/state/tymek-theme
+  '';
 
   programs.wezterm = {
     enable = true;
@@ -22,9 +26,13 @@
 
       config.keys = {
         { key = "w", mods = "SUPER", action = wezterm.action.Nop },
+
+        { key = "t", mods = "SUPER|SHIFT", action = wezterm.action_callback(theme.cycle_theme) },
+        { key = "m", mods = "SUPER|SHIFT", action = wezterm.action_callback(theme.cycle_mode) },
+        { key = "m", mods = "SUPER|SHIFT|META", action = wezterm.action_callback(theme.enable_auto_mode) },
       }
 
-      theme.set(config, false)
+      theme.setup(config)
 
       return config
     '';
