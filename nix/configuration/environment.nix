41e34c251e946c8a5ca6e1efb221c3c79f625a3a
Tymoteusz Makowski <tymek.makowski@gmail.com>
2025-08-04 22:07:22 +0200

    feat(nixos): add missing packages

--- nix/configuration/environment.nix
+++ nix/configuration/environment.nix
@@ -1,10 +1,19 @@
 { pkgs, ... } :
 {
   environment.systemPackages = with pkgs; [
+    bat
     discord
-    gcc
+    fd
+    gcc # used by nvim-treesitter to install grammars
+    gh
+    git-absorb
+    go-task
+    ijq
+    jq
+    ripgrep
     telegram-desktop
     tofi # dynamic menu for hyprland
+    yazi
   ];
 
   environment.variables = {
