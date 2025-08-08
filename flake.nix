1a3f9f4fbf4cfdde4d51d86960ed4111406cb7f3
Tymoteusz Makowski <tymek.makowski@gmail.com>
2025-08-08 15:31:15 +0200

    fix(nixos): import bat.nix in flake.nix

--- flake.nix
+++ flake.nix
@@ -51,6 +51,7 @@
                 xdg.enable = true;
                 imports = [
                   ./nix/home-manager/machines/sffpc.nix
+                  ./nix/home-manager/bat.nix
                   ./nix/home-manager/firefox.nix
                   ./nix/home-manager/fish.nix
                   ./nix/home-manager/git.nix
