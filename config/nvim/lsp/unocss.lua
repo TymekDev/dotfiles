3086899e8e20c210eac3f8eeb3ca4acdc4543239
Tymoteusz Makowski <tymek.makowski@gmail.com>
2025-10-09 10:27:52 +0200

    fix: add workspace_required to avoid error messages on startup
    Note: this requires Neovim >=0.11.1

--- config/nvim/lsp/unocss.lua
+++ config/nvim/lsp/unocss.lua
@@ -1,4 +1,5 @@
 ---@type vim.lsp.Config
 return {
   root_markers = { "unocss.config.js", "unocss.config.ts", "uno.config.js", "uno.config.ts", "slides.md" }, -- slides.md for slidev
+  workspace_required = true, -- See https://github.com/xna00/unocss-language-server/pull/23
 }
