3404f4fb165e5366af365e75dc79b790ab6adcdb
Tymoteusz Makowski <tymek.makowski@gmail.com>
2025-11-06 19:04:12 +0000

    feat: pass OS dotfiles config to the home-manager modules

--- nix/configuration/common/home-manager.nix
+++ nix/configuration/common/home-manager.nix
@@ -1,10 +1,26 @@
 { config, pkgs-unstable, ... }:
+let
+  osDotfiles = config.dotfiles;
+in
 {
   home-manager = {
     useUserPackages = true;
     useGlobalPkgs = true;
     backupFileExtension = "hmbak";
     extraSpecialArgs = { inherit pkgs-unstable; };
-    users.${config.dotfiles.username}.imports = [ ../../home-manager ];
+    users.${config.dotfiles.username}.imports = [
+      ../../home-manager
+
+      (
+        { config, lib, ... }:
+        let
+          osOptions = (import ./config.nix { inherit config lib; }).options;
+        in
+        {
+          options.dotfiles = osOptions.dotfiles;
+          config.dotfiles = osDotfiles;
+        }
+      )
+    ];
   };
 }
