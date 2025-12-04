359149f021dce7f775056060a85e9a7380885664
Tymoteusz Makowski <tymek.makowski@gmail.com>
2025-12-04 22:04:28 +0100

    refactor: use out-of-store symlink for wezterm's theme.lua

--- nix/home-manager/terminals/wezterm/default.nix
+++ nix/home-manager/terminals/wezterm/default.nix
@@ -1,15 +1,24 @@
 {
   config,
+  pkgs,
   pkgs-unstable,
   lib,
   ...
 }:
 let
   inherit (config.dotfiles) hasWezterm;
+  inherit (pkgs.stdenv) isLinux;
+
+  home = if isLinux then "home" else throw "Unsupported OS";
+  mkSymlink =
+    path:
+    config.lib.file.mkOutOfStoreSymlink "/${home}/${config.dotfiles.username}/personal/dotfiles/${path}";
 in
 {
   config = lib.mkIf hasWezterm {
-    xdg.configFile."wezterm/theme.lua".source = ../../../config/wezterm/theme.lua;
+    xdg.configFile = {
+      "wezterm/theme.lua".source = mkSymlink "config/wezterm/theme.lua";
+    };
 
     home.activation.weztermMkdirThemeDir = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
       run mkdir $VERBOSE_ARG --parents $HOME/.local/state/tymek-theme
