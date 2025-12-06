cf359a451cdfcb1ef56b6d349a1f04fe8a3ae744
Tymoteusz Makowski <tymek.makowski@gmail.com>
2025-12-06 20:56:10 +0100

    feat: add a keymapping to run taskfile

--- nix/home-manager/terminals/wezterm/wezterm.lua
+++ nix/home-manager/terminals/wezterm/wezterm.lua
@@ -32,6 +32,55 @@ config.keys = {
       sessionizer.switch_to_last(win, pane)
     end),
   },
+
+  {
+    key = "T",
+    mods = "LEADER",
+    action = wezterm.action_callback(function(win, pane)
+      local dir = pane:get_current_working_dir().file_path
+      local cmd = { "task", "--dir", dir, "--list", "--json" }
+      local ok, stdout, stderr = wezterm.run_child_process(cmd)
+      if not ok then
+        wezterm.log_error("failed to run the command:", stderr, "\nThe command:", cmd)
+        return
+      end
+
+      ---@type InputSelectorItem[]
+      local choices = {}
+      local max_len = 0
+      for _, task in ipairs(wezterm.serde.json_decode(stdout).tasks) do
+        if #task.name > max_len then
+          max_len = #task.name
+        end
+        table.insert(choices, { id = task.name, label = task.desc })
+      end
+
+      for _, choice in ipairs(choices) do
+        choice.label = wezterm.pad_right(choice.id, max_len + 5) .. choice.label
+      end
+
+      win:perform_action(
+        wezterm.action.InputSelector({
+          fuzzy = true,
+          choices = choices,
+          action = wezterm.action_callback(function(_, _, id)
+            if not id then
+              return
+            end
+
+            win:perform_action(
+              -- FIXME: this gets closed immediately after the command is done - it's not ideal.
+              wezterm.action.SpawnCommandInNewTab({
+                args = { "task", "--dir", dir, id },
+              }),
+              pane
+            )
+          end),
+        }),
+        pane
+      )
+    end),
+  },
 }
 
 theme.setup(config)
