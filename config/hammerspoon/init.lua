c9c3d6a040a739b177b0d4e1cfd09a9ea4c035d7
Tymoteusz Makowski <tymek.makowski@gmail.com>
2026-01-08 11:42:00 +0100

    feat: wrap Cmd+W to not close the Windows app

--- config/hammerspoon/init.lua
+++ config/hammerspoon/init.lua
@@ -6,3 +6,22 @@ hs.loadSpoon("EmmyLua")
 -- hs.hotkey.bind({ "cmd" }, "return", function()
 --   hs.application.launchOrFocus("wezterm")
 -- end)
+
+---@type hs.hotkey
+local cmd_w
+cmd_w = hs.hotkey.bind({ "cmd" }, "w", function()
+  local win = hs.window.focusedWindow()
+  if
+    win and win
+      :application()--[[@as hs.application]]
+      :name() == "Windows App"
+  then
+    return
+  end
+
+  cmd_w:disable()
+  hs.eventtap.keyStroke({ "cmd" }, "w")
+  cmd_w:enable()
+end)
+
+hs.hotkey.setLogLevel("warning")
