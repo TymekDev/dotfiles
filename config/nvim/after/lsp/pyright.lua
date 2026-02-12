368448327e220dd56bd7254f0e6c51019ff20cdc
Tymoteusz Makowski <tymek.makowski@gmail.com>
2026-02-12 15:17:32 +0100

    refactor: switch from pylsp to pyright+ruff

---
+++ config/nvim/after/lsp/pyright.lua
@@ -0,0 +1,13 @@
+---@type vim.lsp.Config
+return {
+  settings = {
+    pyright = {
+      disableOrganizeImports = true,
+    },
+    python = {
+      analysis = {
+        ignore = { "*" },
+      },
+    },
+  },
+}
