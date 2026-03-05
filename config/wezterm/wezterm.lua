32c787510dec90c976d0c0f1df2073a63191a1d7
Tymoteusz Makowski <tymek.makowski@gmail.com>
2026-03-05 14:09:39 +0100

    feat: add Taskfile wezterm keymap

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -14,6 +14,7 @@ config.window_padding = {
   left = 0,
 }
 config.window_decorations = "RESIZE"
+config.exit_behavior = "Hold"
 
 local function cmd_to_meta(key)
   return { key = key, mods = "CMD", action = wezterm.action.SendKey({ key = key, mods = "META" }) }
@@ -99,6 +100,53 @@ config.keys = {
       local dir = sessionizer.active_workspace_dir()
       local cmd = shell_cmd({ "opencode", dir })
       win:perform_action(wezterm.action.SpawnCommandInNewTab({ args = cmd }), pane)
+    end),
+  },
+  {
+    key = "T",
+    mods = "LEADER",
+    action = wezterm.action_callback(function(win, pane)
+      local dir = sessionizer.active_workspace_dir()
+      local cmd = shell_cmd({ "task", "--dir", dir, "--list", "--json" })
+      local ok, stdout, stderr = wezterm.run_child_process(cmd)
+      if not ok then
+        wezterm.log_error("failed to run the command:", stderr, "\nThe command:", cmd)
+        return
+      end
+
+      ---@type InputSelectorItem[]
+      local choices = {}
+      local max_len = 0
+      for _, task in ipairs(wezterm.serde.json_decode(stdout).tasks) do
+        if #task.name > max_len then
+          max_len = #task.name
+        end
+        table.insert(choices, { id = task.name, label = task.desc })
+      end
+
+      for _, choice in ipairs(choices) do
+        choice.label = wezterm.pad_right(choice.id, max_len + 5) .. choice.label
+      end
+
+      win:perform_action(
+        wezterm.action.InputSelector({
+          fuzzy = true,
+          choices = choices,
+          action = wezterm.action_callback(function(_, _, id)
+            if not id then
+              return
+            end
+
+            win:perform_action(
+              wezterm.action.SpawnCommandInNewTab({
+                args = shell_cmd({ "task", "--dir", dir, id }),
+              }),
+              pane
+            )
+          end),
+        }),
+        pane
+      )
     end),
   },
 }
