9e3354b20fbb66c68089bb1b84af2e5897dde112
Tymoteusz Makowski <tymek.makowski@gmail.com>
2026-03-06 09:08:43 +0100

    refactor: update copilot.lua configuration

--- config/nvim/lua/tymek/plugins/copilot.lua
+++ config/nvim/lua/tymek/plugins/copilot.lua
@@ -3,20 +3,31 @@
 return {
   "zbirenbaum/copilot.lua",
   cmd = "Copilot",
-  keys = {
-    {
-      mode = "i",
-      "<M-j>",
-      function()
-        require("copilot.suggestion").accept()
-      end,
-    },
-  },
+  event = "InsertEnter",
   ---@module "copilot"
   ---@type CopilotConfig
+  ---@diagnostic disable-next-line missing-fields
   opts = {
+    should_attach = function(_, bufname)
+      if string.match(bufname, "env") then
+        return false
+      end
+      return true
+    end,
+    ---@diagnostic disable-next-line missing-fields
     panel = {
       enabled = false,
+    },
+    ---@diagnostic disable-next-line missing-fields
+    suggestion = {
+      enabled = true,
+      auto_trigger = true,
+      ---@diagnostic disable-next-line missing-fields
+      keymap = {
+        accept = "<M-j>",
+        accept_word = "<M-k>",
+        accept_line = "<M-l>",
+      },
     },
   },
 }
