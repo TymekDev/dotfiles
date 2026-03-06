869f423a430b9977ac50c9940e7c42951d6bede0
Tymoteusz Makowski <tymek.makowski@gmail.com>
2026-03-06 12:00:42 +0100

    fix: symlink the macos-specific wezterm.lua

--- nix/home-manager/terminals/wezterm/default.nix
+++ nix/home-manager/terminals/wezterm/default.nix
@@ -1,5 +1,6 @@
-{ config, ... }:
+{ config, pkgs, lib, ... }:
 let
+  inherit (pkgs.stdenv) isDarwin isLinux;
   mkSymlink =
     path:
     config.lib.file.mkOutOfStoreSymlink "${config.dotfiles.home}/personal/dotfiles/${path}";
@@ -8,10 +9,15 @@ in
   # NOTE: I cannot use ./wezterm.lua and similar, because those files are included as source in
   # /nix/store/ and the out-of-store symlink ends up being an in-store symlink.
   xdg.configFile = {
-    "wezterm/wezterm.lua".source = mkSymlink "nix/home-manager/terminals/wezterm/wezterm.lua";
     "wezterm/sessionizer.lua".source = mkSymlink "config/wezterm/sessionizer.lua";
     "wezterm/tab_bar.lua".source = mkSymlink "config/wezterm/tab_bar.lua";
     "wezterm/theme.lua".source = mkSymlink "config/wezterm/theme.lua";
+  }
+  // lib.optionalAttrs isDarwin {
+    "wezterm/wezterm.lua".source = mkSymlink "config/wezterm/wezterm.lua";
+  }
+  // lib.optionalAttrs isLinux {
+    "wezterm/wezterm.lua".source = mkSymlink "nix/home-manager/terminals/wezterm/wezterm.lua";
   };
 
   programs.wezterm = {
