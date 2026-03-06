6b751e761f0933569a4045e70cebcf8959a7f25d
Tymoteusz Makowski <tymek.makowski@gmail.com>
2026-03-06 09:06:36 +0100

    refactor: disable todo-comments.nvim

--- config/nvim/lua/tymek/plugins/todo-comments.lua
+++ config/nvim/lua/tymek/plugins/todo-comments.lua
@@ -2,6 +2,7 @@
 ---@type LazySpec
 return {
   "folke/todo-comments.nvim", -- TODO: review config
+  enabled = false,
   event = "VeryLazy",
   dependencies = { "nvim-lua/plenary.nvim" },
   ---@module "todo-comments"
