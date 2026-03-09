05ff8f75017608098cebe897226fa28a9fa20f93
Tymoteusz Makowski <tymek.makowski@gmail.com>
2026-03-09 14:06:46 +0100

    refactor: migrate direnv config to nix

--- nix/home-manager/direnv.nix
+++ nix/home-manager/direnv.nix
@@ -1,8 +1,33 @@
-{ ... }:
+{ pkgs, ... }:
 {
   programs.direnv = {
     enable = true;
 
+    enableFishIntegration = true;
+
     nix-direnv.enable = true;
   };
+
+  xdg.configFile."direnv/lib/python.sh".source = pkgs.writeShellScript "python.sh" ''
+    layout_uv() {
+      if [[ ! -d ".venv" ]]; then
+        log_status "Creating venv with uv..."
+        uv venv
+      fi
+
+      VIRTUAL_ENV="$(pwd)/.venv"
+      PATH_add "$VIRTUAL_ENV/bin"
+      export VIRTUAL_ENV
+
+      if [[ -f "uv.lock" ]]; then
+        watch_file uv.lock
+      fi
+
+      if [[ -f "pyproject.toml" ]]; then
+        watch_file pyproject.toml
+        log_status "Syncing dependencies with uv..."
+        uv sync
+      fi
+    }
+  '';
 }
