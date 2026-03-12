d441189692d7040288d9ead06b1eae1f1489ef27
Tymoteusz Makowski <tymek.makowski@gmail.com>
2026-03-12 12:31:43 +0100

    feat(nix-darwin): port timezone and hostname to Nix

--- nix/machines/maczek/default.nix
+++ nix/machines/maczek/default.nix
@@ -1,6 +1,16 @@
 { ... }:
+let
+  name = "maczek";
+  tz = "Europe/Warsaw";
+in
 {
+  networking.computerName = name;
+  networking.hostName = name;
+
   nixpkgs.hostPlatform = "aarch64-darwin";
+
+  environment.variables.TZ = tz;
+  time.timeZone = tz;
 
   system.stateVersion = 6;
 }
