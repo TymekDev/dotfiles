6560d6372371ce3b9d5c7846e4edde033bcabd11
Tymoteusz Makowski <tymek.makowski@gmail.com>
2026-03-12 09:01:52 +0100

    fix: add SSH_AUTH_SOCK

--- nix/home-manager/default.nix
+++ nix/home-manager/default.nix
@@ -27,6 +27,10 @@ in
     ./starship.nix
   ];
 
+  home.sessionVariables = lib.optionalAttrs isDarwin {
+    SSH_AUTH_SOCK = "~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock";
+  };
+
   home.packages =
     with pkgs;
     [
