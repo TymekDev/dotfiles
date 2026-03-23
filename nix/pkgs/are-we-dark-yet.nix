47c6eb61d8084ece2998da420ff80ccae7d5214e
Tymoteusz Makowski <tymek.makowski@gmail.com>
2026-03-23 21:50:22 +0100

    refactor: unnest

--- nix/pkgs/are-we-dark-yet.nix
+++ nix/pkgs/are-we-dark-yet.nix
@@ -9,13 +9,11 @@ writeShellApplication {
   runtimeInputs = lib.optionals stdenv.isLinux [ dbus ];
   text = ''
     OS="$(uname -s)"
-    if [ "$OS" = "Darwin" ]; then
+    if [ -n "''${CODESPACES:-}" ]; then
+      echo "light" # I use Codespaces at work during the day
+    elif [ "$OS" = "Darwin" ]; then
       defaults read -g AppleInterfaceStyle &>/dev/null && echo "dark" || echo "light"
     elif [ "$OS" = "Linux" ]; then
-      if [ -n "''${CODESPACES:-}" ]; then
-        echo "light" # I use Codespaces at work during the day
-        exit 0
-      fi
       dbus-send --session --print-reply=literal --reply-timeout=1000 --dest=org.freedesktop.portal.Desktop /org/freedesktop/portal/desktop org.freedesktop.portal.Settings.Read string:"org.freedesktop.appearance" string:"color-scheme" | grep -q "uint32 1" && echo "dark" || echo "light"
     else
       echo "are-we-dark-yet: unsupported OS: $OS"
