fdcec4409205c74cebf4ab50fc14814bdaf6b6c5
Tymoteusz Makowski <tymek.makowski@gmail.com>
2026-03-23 13:50:07 +0100

    feat: add rebuild command

--- flake.nix
+++ flake.nix
@@ -36,6 +36,7 @@
 
   outputs =
     {
+      self,
       nixpkgs-unstable,
       nix-darwin,
       disko,
@@ -122,6 +123,25 @@
         {
           codespace = mkConfig "codespace";
           vscode = mkConfig "vscode";
+        };
+
+      apps.${system}.rebuild-codespace =
+        let
+          pkgs = import nixpkgs-unstable { system = "x86_64-linux"; };
+        in
+        {
+          type = "app";
+          program = toString (
+            pkgs.writeShellScript "codespace-switch" ''
+              set -euo pipefail
+              exec nix \
+                run github:nix-community/home-manager \
+                --experimental-features 'flakes nix-command' \
+                -- \
+                switch --flake "${self}#''$(whoami)" \
+                --experimental-features 'flakes nix-command'
+            ''
+          );
         };
     };
 }
