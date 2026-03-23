69b604681e328e2285d4b2893193ba4129cad69f
Tymoteusz Makowski <tymek.makowski@gmail.com>
2026-03-23 15:24:15 +0100

    refactor: use nix.conf

--- nix/pkgs/nix-codespace-rebuild.nix
+++ nix/pkgs/nix-codespace-rebuild.nix
@@ -4,10 +4,7 @@
 writeShellApplication {
   name = "nix-codespace-rebuild";
   text = ''
-    nix run github:nix-community/home-manager \
-      --experimental-features "flakes nix-command" \
-      -- \
-      switch --flake "git+https://code.tymek.dev/TymekDev/dotfiles#$(id -un)" \
-      --experimental-features "flakes nix-command"
+    nix run github:nix-community/home-manager -- \
+      switch --flake "git+https://code.tymek.dev/TymekDev/dotfiles#$(id -un)"
   '';
 }
