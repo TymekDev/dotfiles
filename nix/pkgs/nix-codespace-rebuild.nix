a98a9057e27fdf6e853514f0c918f8e0e8b10f94
Tymoteusz Makowski <tymek.makowski@gmail.com>
2026-03-23 16:06:21 +0100

    feat: support providing ref in nix-codespace-rebuild

--- nix/pkgs/nix-codespace-rebuild.nix
+++ nix/pkgs/nix-codespace-rebuild.nix
@@ -4,7 +4,12 @@
 writeShellApplication {
   name = "nix-codespace-rebuild";
   text = ''
+    REF=""
+    if [ -n "''${1:-}" ]; then
+      REF="?ref=$1"
+    fi
+
     nix run github:nix-community/home-manager -- \
-      switch --flake "git+https://code.tymek.dev/TymekDev/dotfiles#$(id -un)"
+      switch --flake "git+https://code.tymek.dev/TymekDev/dotfiles''${REF}#$(id -un)"
   '';
 }
