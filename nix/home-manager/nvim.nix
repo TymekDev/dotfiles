d30eeafc9fddc9e2e7177ebb955831b20c1ba463
Tymoteusz Makowski <tymek.makowski@gmail.com>
2026-03-23 14:30:04 +0100

    refactor: use read-only directory in codespaces

--- nix/home-manager/nvim.nix
+++ nix/home-manager/nvim.nix
@@ -4,11 +4,12 @@
   ...
 }:
 let
+  inherit (config.dotfiles) isCodespace;
   mkSymlink =
     path: config.lib.file.mkOutOfStoreSymlink "${config.dotfiles.home}/personal/dotfiles/${path}";
 in
 {
-  xdg.configFile."nvim".source = mkSymlink "config/nvim";
+  xdg.configFile."nvim".source = if isCodespace then ../../config/nvim else mkSymlink "config/nvim";
 
   home.packages = with pkgs; [
     gcc # used by nvim-treesitter to install grammars
