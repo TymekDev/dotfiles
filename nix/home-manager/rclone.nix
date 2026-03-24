a89569b800595396bf350197de4051468b8b27be
Tymoteusz Makowski <tymek.makowski@gmail.com>
2026-03-24 09:00:43 +0100

    refactor: create rclone.conf from the remotes on nix-darwin

--- nix/home-manager/rclone.nix
+++ nix/home-manager/rclone.nix
@@ -7,29 +7,48 @@
 let
   inherit (config.dotfiles) isCodespace;
   inherit (pkgs.stdenv) isDarwin;
-in
-{
-  xdg.configFile."rclone/rclone.conf" = lib.mkIf isDarwin {
-    source = ../../config/rclone/rclone.conf;
-  };
 
-  programs.rclone = {
-    enable = !isCodespace;
-
-    remotes = {
-      helix.config = {
-        type = "sftp";
+  remotes =
+    let
+      sftpConfig =
+        {
+          host,
+          user,
+          port,
+        }:
+        {
+          inherit host user port;
+          type = "sftp";
+          shell_type = "unix";
+          md5sum_command = "md5sum";
+          sha1sum_command = "sha1sum";
+        };
+    in
+    {
+      helix.config = sftpConfig {
         host = "helix.lambda.town";
         user = "tymek";
         port = 2116;
       };
 
-      hetzner.config = {
-        type = "sftp";
+      hetzner.config = sftpConfig {
         host = "u375346.your-storagebox.de";
         user = "u375346";
         port = 23;
       };
     };
+in
+{
+  # NOTE: this is needed for nix-darwin as the rclone home-manager module uses systemd to update the config file.
+  xdg.configFile."rclone/rclone.conf" = lib.mkIf isDarwin {
+    source = (pkgs.formats.ini { }).generate "rclone.conf" (
+      builtins.mapAttrs (name: value: value.config) remotes
+    );
+  };
+
+  programs.rclone = {
+    enable = !isCodespace;
+
+    inherit remotes;
   };
 }
