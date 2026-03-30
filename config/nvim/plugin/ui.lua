9c7ed1e5e8c69e25d7528baf79b534781eff6289
Tymoteusz Makowski <tymek.makowski@gmail.com>
2026-03-30 23:15:55 +0200

    refactor: migrate gitsigns.nvim and quicker.nvim to vim.pack

--- config/nvim/plugin/ui.lua
+++ config/nvim/plugin/ui.lua
@@ -57,3 +57,92 @@ vim.schedule(function()
   vim.pack.add({ "https://github.com/folke/todo-comments.nvim" }, { confirm = false })
   require("todo-comments").setup({ signs = false })
 end)
+
+vim.schedule(function()
+  vim.pack.add({ "https://github.com/lewis6991/gitsigns.nvim" }, { confirm = false })
+  require("gitsigns").setup({
+    on_attach = function(bufnr)
+      vim.keymap.set(
+        { "n", "v" },
+        "[g",
+        "<Cmd>Gitsigns prev_hunk<CR>",
+        { buffer = bufnr, desc = "Jump to the previous git hunk (via gitsigns.nvim)" }
+      )
+      vim.keymap.set(
+        { "n", "v" },
+        "]g",
+        "<Cmd>Gitsigns next_hunk<CR>",
+        { buffer = bufnr, desc = "Jump to the next git hunk (via gitsigns.nvim)" }
+      )
+    end,
+  })
+end)
+
+vim.schedule(function()
+  vim.pack.add({ "https://github.com/stevearc/quicker.nvim" }, { confirm = false })
+  require("quicker").setup({
+    trim_leading_whitespace = false,
+    use_default_opts = false,
+    max_filename_width = function()
+      return 30
+    end,
+    keys = {
+      {
+        ">",
+        function()
+          require("quicker").expand({ before = 2, after = 2, add_to_existing = true })
+        end,
+        desc = "Expand quickfix context (via quicker.nvim)",
+      },
+      {
+        "<",
+        function()
+          require("quicker").collapse()
+        end,
+        desc = "Collapse quickfix context (via quicker.nvim)",
+      },
+      {
+        "]q",
+        function()
+          local items = vim.fn.getqflist()
+          local lnum = vim.api.nvim_win_get_cursor(0)[1]
+          for i = lnum + 1, #items do
+            if items[i].valid == 1 then
+              vim.api.nvim_win_set_cursor(0, { i, 0 })
+              return
+            end
+          end
+          -- Wrap around the end of quickfix list
+          for i = 1, lnum do
+            if items[i].valid == 1 then
+              vim.api.nvim_win_set_cursor(0, { i, 0 })
+              return
+            end
+          end
+        end,
+        desc = "Go to the next entry without jumping to its location (via quicker.nvim)",
+      },
+      {
+        "[q",
+        function()
+          local items = vim.fn.getqflist()
+          local lnum = vim.api.nvim_win_get_cursor(0)[1]
+          for i = lnum - 1, 1, -1 do
+            if items[i].valid == 1 then
+              vim.api.nvim_win_set_cursor(0, { i, 0 })
+              return
+            end
+          end
+          -- Wrap around the start of quickfix list
+          for i = #items, lnum, -1 do
+            if items[i].valid == 1 then
+              vim.api.nvim_win_set_cursor(0, { i, 0 })
+              return
+            end
+          end
+        end,
+        desc = "Go to the previous entry without jumping to its location (via quicker.nvim)",
+      },
+    },
+  })
+end)
