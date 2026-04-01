9e379a35307217ae6c074ba382751eba76021222
Tymoteusz Makowski <tymek.makowski@gmail.com>
2026-04-01 10:05:23 +0200

    build: add language servers and formatters to Nix

--- nix/home-manager/nvim.nix
+++ nix/home-manager/nvim.nix
@@ -18,13 +18,26 @@ in
       tree-sitter # TODO: switch to installing compiled grammars directly?
 
       # Language servers
+      astro-language-server
+      bash-language-server
+      emmet-language-server
+      fish-lsp
       lua-language-server
       nixd
+      tailwindcss-language-server
+      taplo
+      typescript-language-server
       vscode-langservers-extracted
+      yaml-language-server
 
       # Formatters
       nixfmt
+      prettierd
+      ruff
+      shellcheck
+      shfmt
       stylua
+      yamlfmt
     ]
     ++ lib.optionals (!isCodespace) [
       gcc # used by nvim-treesitter to install grammars
