e446178a52e33de1322107b54fa5f47c3d309bb5
Tymoteusz Makowski <tymek.makowski@gmail.com>
2026-04-02 13:00:05 +0200

    refactor: migrate R-related commands to ftplugin

--- config/nvim/after/ftplugin/r.lua
+++ config/nvim/after/ftplugin/r.lua
@@ -18,6 +18,39 @@ end, {
 vim.keymap.set("i", "<M-,>", " <- ")
 vim.keymap.set("n", "<Leader>bi", ts_r.put_missing_box_imports, { buffer = 0 })
 
+local run_lintr = function(code)
+  vim.system(
+    {
+      "Rscript",
+      "-e",
+      code,
+    },
+    {},
+    vim.schedule_wrap(function(result)
+      local lines = vim.split(result.stdout, "\n")
+
+      local ok, msg = pcall(vim.fn.setqflist, {}, " ", { lines = lines, efm = [[%f:%l:%c:\ %m]] })
+      if not ok then
+        vim.notify(msg --[[@as string]], vim.log.levels.ERROR)
+      end
+
+      vim.cmd.copen()
+    end)
+  )
+end
+
+vim.api.nvim_create_user_command("RLintPackage", function()
+  run_lintr("lintr::lint_package()")
+end, {
+  desc = "Run lintr::lint_package() and populate the quickfix list with the results",
+})
+
+vim.api.nvim_create_user_command("RLintRhino", function()
+  run_lintr("rhino::lint_r()")
+end, {
+  desc = "Run rhino::lint_r() and populate the quickfix list with the results",
+})
+
 vim.api.nvim_create_autocmd({ "BufWinEnter", "TextChanged", "TextChangedI" }, {
   desc = "Highlight reactives and eventReactives declared in the current buffer",
   buffer = 0,
